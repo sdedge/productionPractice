97c04c1942d84541501b4b771832e5e07c6e47c5
KOPOBA <dima.peven@yandex.ru>
2022-06-04 23:12:20 +0300

    update mainwindow.h
    Добавление класса сервера

--- practiceServer/mainwindow.h
+++ practiceServer/mainwindow.h
@@ -3,6 +3,12 @@
 
 #include <QMainWindow>
 
+///  ========================    классы для работы сервера
+#include <QTcpServer>
+#include <QTcpSocket>
+///  ========================
+#include <QVector>        //    класс вектора для хранения созданных сокетов
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -18,4 +24,23 @@ public:
 private:
     Ui::MainWindow *ui;
 };
+
+class Server : public QTcpServer{   //  создание класса сервера, унаследованного от QTcpServer'a
+    Q_OBJECT
+
+public:
+    Server();
+    QTcpSocket *socket;
+
+private:
+    QVector <QTcpSocket> Sockets;   //  вектор, предназначенный под сокеты
+    QByteArray Data;    //  то, что будет путешествовать между клиентом и сервером
+
+    void SendToClient();    //  функция для передачи данных клиенту
+
+public slots:
+    void incomingConnection(qintptr socketDescriptor);  //  обработчик новых подключений
+    void slotReadyRead();   //  обработчик полученных от клиента сообщений
+};
+
 #endif // MAINWINDOW_H
