520fe238bb0423dbb377b490f31302ab3a0ceb9d
KOPOBA <dima.peven@yandex.ru>
2022-06-07 00:04:32 +0300

    Комментарии и логические дополнения

--- practiceClient/mainwindow.h
+++ practiceClient/mainwindow.h
@@ -22,27 +22,27 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_connectToServerPushButton_clicked();
+    void on_connectToServerPushButton_clicked();    //  нажатие на кнопку "Connect to server"
 
-    void on_sendPushButton_clicked();
+    void on_sendPushButton_clicked();   //  нажатие на кнопку "Send"
 
-    void on_lineEdit_returnPressed();
+    void on_lineEdit_returnPressed();   //  событие нажатия клавиши Enter
 
-    void on_openFilePushButton_clicked();
+    void on_openFilePushButton_clicked();   //  нажатие на кнопку "Or open file"
 
 private:
     Ui::MainWindow *ui;
-    QTcpSocket *socket;
-    QByteArray Data;
+    QTcpSocket *socket; //  сокет соединения
+    QByteArray Data;    //  передаваемые файлы
 
-    QCompleter *completer;
-    QFileSystemModel *fModel;
+    QCompleter *completer;  //  автодополнение вводимого пути файла
+    QFileSystemModel *fModel;   //  файловая модель
 
-    void SendToServer(QString str);
+    void SendToServer(QString str); //  метод отправки на сервер сообщения
 
     quint16 nextBlockSize;  //  переменная для хранения размера блока текста
 
 public slots:
-    void slotReadyRead();
+    void slotReadyRead();   //  слот готовности к чтению сообщений
 };
 #endif // MAINWINDOW_H
