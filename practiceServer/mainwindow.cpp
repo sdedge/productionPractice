93a4014d8140aa213dd5d24cfd274e3567ed4f99
KOPOBA <dima.peven@yandex.ru>
2022-06-08 22:48:28 +0300

    Добавление комментариев

--- practiceServer/mainwindow.cpp
+++ practiceServer/mainwindow.cpp
@@ -37,10 +37,11 @@ void MainWindow::slotStatusServer(QString status)
 }
 
 
-void MainWindow::on_chooseSaveDirPushButton_clicked()
+void MainWindow::on_chooseSaveDirPushButton_clicked()   //  по нажатию на "Choose save directory"
 {
-    QString dirPath = QFileDialog::getExistingDirectory(0, "Выбор папки", "");
+    QString dirPath = QFileDialog::getExistingDirectory(0, "Выбор папки", "");  //  выбираем папку
+    //  указываем в статусе сервера, что была изменена директория. HTML тут работает, пользуемся
     ui->infoAboutServerTextEdit->append("<font color = red>!!!<\\font> <br/> <font color = black><\\font>Установлена новая директория сохранения: "+dirPath+"<br/><font color = red>!!!<\\font>");
-    ui->dirPathLabel->setText(dirPath);
+    ui->dirPathLabel->setText(dirPath); //  для наглядности выводим путь в dirPathLabel
 }
 
