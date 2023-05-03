c6478515debc1907701b77a09523ffabe159461d
KOPOBA <dima.peven@yandex.ru>
2023-05-03 20:51:07 +0300

    Перенос логики в SettingsManager и реорганизация переменных

--- practiceServer/helperClasses/managers/workspaceManager/workspace_manager.cpp
+++ practiceServer/helperClasses/managers/workspaceManager/workspace_manager.cpp
@@ -3,47 +3,28 @@
 WorkspaceManager::WorkspaceManager(QString incomingRootFolder)
 {
     this->rootFolder = incomingRootFolder;
+
+    this->settingsFolder = incomingRootFolder+"/Settings";
+    this->dataFolder = incomingRootFolder+"/Data";
+    this->entryFolder = incomingRootFolder+"/Data/Entry";
+    this->expectationFolder = incomingRootFolder+"/Data/Expectation";
+    this->storageFolder = incomingRootFolder+"/Data/Storage";
+
+    m_settingsManager = new SettingsManager(settingsFolder);
 }
 
 QString WorkspaceManager::createWorkspaceFolders()
 {
-    QDir().mkdir(rootFolder+"/Settings");
-    QDir().mkdir(rootFolder+"/Data");
-    QDir().mkdir(rootFolder+"/Data/Entry");
-    QDir().mkdir(rootFolder+"/Data/Expectation");
-    QDir().mkdir(rootFolder+"/Data/Storage");
-
-    QFile possibleProcessingFile(rootFolder+"/Settings/possibleProcessing.json");
-
-    if (!possibleProcessingFile.open(QIODevice::WriteOnly)) {
-        return QString("<hr/>Рабочая папка организована, но проблемы с файлом обработок");
-    }
-
-    QJsonObject m_currentJsonObject;
-
-    possibleProcessingFile.write(QJsonDocument(m_currentJsonObject).toJson(QJsonDocument::Indented));
-
-    possibleProcessingFile.close();
+    QDir().mkdir(settingsFolder);
+    QDir().mkdir(dataFolder);
+    QDir().mkdir(entryFolder);
+    QDir().mkdir(expectationFolder);
+    QDir().mkdir(storageFolder);
 
     return QString("<hr/>Рабочая папка организована!");
 }
 
 QString WorkspaceManager::saveSettings(QJsonObject m_currentJsonObject)
 {
-    QString saveFileName = "serverSettings.json";
-    QFileInfo fileInfo(rootFolder+"/Settings/"+saveFileName);
-    // установим текущую рабочую директорию, где будет файл, без QFileInfo может не заработать
-    QDir::setCurrent(fileInfo.path());
-    // Создаём объект файла и открываем его на запись
-    QFile jsonFile(saveFileName);
-    if (!jsonFile.open(QIODevice::WriteOnly))
-    {
-        return QString("<hr/>Не удалось сохранить настройки");
-    }
-
-    // Записываем текущий объект Json в файл
-    jsonFile.write(QJsonDocument(m_currentJsonObject).toJson(QJsonDocument::Indented));
-    jsonFile.close();
-
-    return QString("<hr/>Файл настроек сохранён");
+    return m_settingsManager->setSettings(m_currentJsonObject);
 }
