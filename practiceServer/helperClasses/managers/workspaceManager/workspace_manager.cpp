72e5d2679d198223ba812680b949c992676df199
KOPOBA <dima.peven@yandex.ru>
2023-05-05 01:46:07 +0300

    Изменение логики класса
    +setRootFolder - устанавливает значения для переменных
    +workspaceFileChanged - слот для обработки измененных директорий

--- practiceServer/helperClasses/managers/workspaceManager/workspace_manager.cpp
+++ practiceServer/helperClasses/managers/workspaceManager/workspace_manager.cpp
@@ -1,16 +1,8 @@
 #include "workspace_manager.h"
 
-WorkspaceManager::WorkspaceManager(QString incomingRootFolder)
+WorkspaceManager::WorkspaceManager()
 {
-    this->rootFolder = incomingRootFolder;
 
-    this->settingsFolder = incomingRootFolder+"/Settings";
-    this->dataFolder = incomingRootFolder+"/Data";
-    this->entryFolder = incomingRootFolder+"/Data/Entry";
-    this->expectationFolder = incomingRootFolder+"/Data/Expectation";
-    this->storageFolder = incomingRootFolder+"/Data/Storage";
-
-    m_settingsManager = new SettingsManager(settingsFolder);
 }
 
 QString WorkspaceManager::createWorkspaceFolders()
@@ -27,4 +19,28 @@ QString WorkspaceManager::createWorkspaceFolders()
 QString WorkspaceManager::saveSettings(QJsonObject m_currentJsonObject)
 {
     return m_settingsManager->setSettings(m_currentJsonObject);
+}
+
+void WorkspaceManager::setRootFolder(QString incomingRootFolder)
+{
+    this->rootFolder = incomingRootFolder;
+
+    this->settingsFolder = incomingRootFolder+"/Settings";
+    this->dataFolder = incomingRootFolder+"/Data";
+    this->entryFolder = incomingRootFolder+"/Data/Entry";
+    this->expectationFolder = incomingRootFolder+"/Data/Expectation";
+    this->storageFolder = incomingRootFolder+"/Data/Storage";
+
+    this->workspaceWatcher = new QFileSystemWatcher();
+
+    workspaceWatcher->addPath(settingsFolder);
+    qDebug() << settingsFolder << workspaceWatcher->directories();
+    connect(workspaceWatcher, &QFileSystemWatcher::directoryChanged, this, &WorkspaceManager::workspaceFileChanged);
+
+    m_settingsManager = new SettingsManager(settingsFolder);
+}
+
+void WorkspaceManager::workspaceFileChanged(const QString &fileName)
+{
+    WorkspaceManager::updateUiComboBoxSignal(fileName);
 }
