9d384346c79e144a5882ab245ffeac9c9a6e898a
KOPOBA <dima.peven@yandex.ru>
2023-05-05 01:39:05 +0300

    Добавление комментариев и изменение логики
    Теперь класс ничего не принимает, работая через метод setRootFolder
    
    Связка с формой происходит посредством сигнало-слотового соединения

--- practiceServer/helperClasses/managers/workspaceManager/workspace_manager.h
+++ practiceServer/helperClasses/managers/workspaceManager/workspace_manager.h
@@ -5,30 +5,53 @@
 ///     Конструктор принимает абсолютный путь папки, над которой требуется менеджмент
 ///     Переменные:
 ///     rootFolder - абсолютный путь рабочей директории
+///     settingsFolder - путь до папки Settings
+///     dataFolder - путь до папки Data
+///     entryFolder - путь до папки Entry
+///     expectationFolder - путь до папки Excectation
+///     storageFolder - путь до папки Storage
+///     parentUi - ссылка на родителя
+///     m_settingsManager - экземпляр менеджера папки Settings
+///     workspaceWatcher - наблюдатель за состоянием рабочего пространства
 ///     Методы:
 ///     createWorkspaceFolders() - создаёт структуру папки и возвращает отчёт в консоль
 ///     saveSettings() - принимает json объект и создаёт из него файл.json, возвращая отчёт в консоль
+///     setRootFolder() - устанавливает новые пути для папок
+///     Слоты:
+///     workspaceFileChanged() - обработка сигнала от QFileSystemWatcher::directoryChanged
+///     Сигналы:
+///     updateUiComboBoxSignal() - сигнал для смены данных в ComboBox обработок
 
 ///  ========================    классы проекта
 #include "helperClasses/managers/workspaceManager/settingsManager/settings_manager.h"   //  менеджер для папки /Settings
 ///  ========================
 ///
 ///  ========================    классы для работы с директориями
-#include <QDir>             //  для работы с директориями
-#include <QFile>            //  для работы с файлами
+#include <QDir>                 //  для работы с директориями
+#include <QFile>                //  для работы с файлами
+#include <QFileSystemWatcher>   //  для наблюдения за состоянием папки
 ///  ========================
 ///
 ///  ========================    классы для работы с json
-#include <QJsonDocument>    //  для создания json документов
-#include <QJsonObject>      //  для использования переменных типа QJsonObject
+#include <QJsonDocument>        //  для создания json документов
+#include <QJsonObject>          //  для использования переменных типа QJsonObject
+///  ========================
+///
+///  ========================    классы для работы класса
+#include <QObject>              //  класс для работы connect()
 
-class WorkspaceManager
+class WorkspaceManager : public QObject
 {
+    Q_OBJECT
 public:
-    WorkspaceManager(QString incomingRootFolder);
+    WorkspaceManager();
 
     QString createWorkspaceFolders();
     QString saveSettings(QJsonObject m_currentJsonObject);
+    void setRootFolder(QString incomingRootFolder);
+
+signals:
+    void updateUiComboBoxSignal(const QString &fileName);
 
 private:
     QString rootFolder;
@@ -38,7 +61,12 @@ private:
     QString expectationFolder;
     QString storageFolder;
 
+    QFileSystemWatcher *workspaceWatcher;
+
     SettingsManager *m_settingsManager;
+
+private slots:
+    void workspaceFileChanged(const QString &fileName);
 };
 
 #endif // WORKSPACEMANAGER_H
