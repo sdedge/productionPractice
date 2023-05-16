d5be07d512a47747f9858bfd0dc1e0c1dd007f2b
KOPOBA <dima.peven@yandex.ru>
2023-05-16 20:39:57 +0300

    Добавление функционала

--- practiceServer/helperClasses/managers/readyReadManager/supportRRManagers/I_message_manager.h
+++ practiceServer/helperClasses/managers/readyReadManager/supportRRManagers/I_message_manager.h
@@ -3,17 +3,24 @@
 
 ///     Класс I_MessageManager является интерфейсом для обработчиков сообщений слота сервера ReadyRead
 ///     Методы:
+///     readDataFromStream() - чтение данных с потока
+///     writeDataFromStream() - запись данных в поток
 ///     processData() - обрабатывает приходящие данные
 ///     typeOfMessage() - возвращает строку тип менеджера
 
-#include <QObject>  //  для создания объекта
+///  ========================    классы для работы
+#include <QObject>              //  для создания объекта
+#include <QTcpSocket>           //  для работы с сокетом
+#include <QDebug>               //  для дебага
 
 class I_MessageManager : public QObject
 {
     Q_OBJECT
 public:
     I_MessageManager();
-    virtual void processData(QDataStream &in) = 0;
+    virtual void readDataFromStream(QDataStream &inStream) = 0;
+    virtual void writeDataToStream(QDataStream &outStream) = 0;
+    virtual void processData(QDataStream &inStream, QTcpSocket *socket) = 0;
     virtual QString typeOfMessage() = 0;
 };
 
