03a5e17580ddba3fdf29866d4733a9c4449ffde2
KOPOBA <dima.peven@yandex.ru>
2023-05-16 20:33:35 +0300

    Комментарии и менеджеры, работа со слотами
    1. Добавление комментариев
    2. Создание менеджеров и их связка
    3. Реализация слотов

--- practiceServer/helperClasses/managers/readyReadManager/ready_read_manager.cpp
+++ practiceServer/helperClasses/managers/readyReadManager/ready_read_manager.cpp
@@ -3,17 +3,50 @@
 ReadyReadManager::ReadyReadManager()
 {
     clientsMessageManager = new ClientsMessageManager();
-    nullManager = new NullManager();
+    connect(clientsMessageManager, &ClientsMessageManager::signalStatusRRManager, this, &ReadyReadManager::slotStatusRRManager);
+    connect(clientsMessageManager, &ClientsMessageManager::signalSendToAllClientsRRManager, this, &ReadyReadManager::slotSendToAllClientsRRManager);
 
+    clientsFileManager = new ClientsFileManager();
+    connect(clientsFileManager, &ClientsFileManager::signalStatusRRManager, this, &ReadyReadManager::slotStatusRRManager);
+    connect(clientsFileManager, &ClientsFileManager::signalSendToOneRRManager, this, &ReadyReadManager::slotSendToOneRRManager);
+
+    nullManager = new NullManager();
     messageManagers[clientsMessageManager->typeOfMessage()] = clientsMessageManager;
+    messageManagers[clientsFileManager->typeOfMessage()] = clientsFileManager;
 }
 
-I_MessageManager *ReadyReadManager::identifyMessage(QString message)
+I_MessageManager *ReadyReadManager::identifyMessage(QString typeOfMess)
 {
+    /// Создать список всех typeOfMess и пробегаться по нему
+    /// При совпадении создавать new конструктор()
+    /// иначе возвращать nullManager
     for(auto it = messageManagers.begin(); it != messageManagers.end(); it++){
-        if(it.key() == message){
+        if(it.key() == typeOfMess){
             return it.value();
         }
     }
     return nullManager;
+}
+
+void ReadyReadManager::setEntryFolder(QString &entryFolder)
+{
+    clientsFileManager->setEntryFolderName(entryFolder);
+}
+
+void ReadyReadManager::slotStatusRRManager(QString status)
+{
+    qDebug() << "ReadyReadManager::slotStatusRRManager:     " << status;
+    emit signalStatusRRManagerServer(status);
+}
+
+void ReadyReadManager::slotSendToAllClientsRRManager(QString typeOfMsg, QString str)
+{
+    qDebug() << "ReadyReadManager::slotSendToAllClientsRRManager:     " << typeOfMsg << " | " << str;
+    emit signalSendToAllClientsServer(typeOfMsg, str);
+}
+
+void ReadyReadManager::slotSendToOneRRManager(QTcpSocket *socket, QString typeOfMsg, QString str)
+{
+    qDebug() << "ReadyReadManager::slotSendToOneRRManager:     " << typeOfMsg << " | " << str;
+    emit signalSendToAllClientsServer(typeOfMsg, str);
 }
--- practiceServer/helperClasses/managers/readyReadManager/ready_read_manager.h
+++ practiceServer/helperClasses/managers/readyReadManager/ready_read_manager.h
@@ -5,14 +5,25 @@
 ///     Переменные:
 ///     messageManagers - хранит название обработчика и его указатель
 ///     clientsMessageManager - указатель на менеджер типа "Message"
+///     clientsFileManager - указатель на менеджер типа "File"
 ///     nullManager - указатель на менеджер типа "No type"
 ///     Методы:
 ///     identifyMessage() - определяет тип сообщения и возвращает указатель на необходимого обработчика
+///     setEntryFolder() - установки EntryFolder для ClientsFileManager
+///     Сигналы:
+///     signalStatusRRManagerServer() - отправляет серверу статус
+///     signalSendToAllClientsServer() - отправляет сообщение всем клиентам, обращаясь к серверу
+///     signalSendToOneRRManager() - отправляет сообщение указанному клиенту, обращаясь к серверу
+///     Слоты:
+///     slotStatusRRManager() - принимает статусы на сервер от дочерних менеджеров
+///     slotSendToAllClientsRRManager() - принимает сообщения для всех клиентов от дочерних менеджеров
+///     slotSendToOneRRManager() - принимает сообщения для указанного сокета от дочерних менеджеров
 
 ///  ========================    классы проекта
 #include "supportRRManagers/I_message_manager.h"    //  для работы с ссылками обработчиков
 #include "supportRRManagers/nullManager/null_manager.h"    //  для определения менеджера "заглушки"
 #include "supportRRManagers/clientsMessageManager/clients_message_manager.h"    //  для определения менеджера "Message"
+#include "supportRRManagers/clientsFileManager/clients_file_manager.h"  //  для определения менеджера "File"
 ///  ========================
 ///
 ///  ========================    классы для работы
@@ -25,11 +36,23 @@ class ReadyReadManager : public QObject
 private:
     QMap<QString, I_MessageManager*> messageManagers;
 
-    I_MessageManager *clientsMessageManager;
-    I_MessageManager *nullManager;
+    ClientsMessageManager *clientsMessageManager;
+    ClientsFileManager *clientsFileManager;
+    NullManager *nullManager;
 public:
     ReadyReadManager();
-    I_MessageManager* identifyMessage(QString message);
+    I_MessageManager* identifyMessage(QString typeOfMess);
+    void setEntryFolder(QString &entryFolder);
+
+signals:
+    void signalStatusRRManagerServer(QString status);
+    void signalSendToAllClientsServer(QString typeOfMsg, QString str);
+    void signalSendToOneRRManager(QTcpSocket* socket, QString typeOfMsg, QString str);
+
+private slots:
+    void slotStatusRRManager(QString status);
+    void slotSendToAllClientsRRManager(QString typeOfMsg, QString str);
+    void slotSendToOneRRManager(QTcpSocket* socket, QString typeOfMsg, QString str);
 };
 
 #endif // READYREADMANAGER_H
