69ebe32584ab66274f9bbdbacaa3ccc417793bf3
KOPOBA <dima.peven@yandex.ru>
2023-05-16 20:38:52 +0300

    Комментарии, добавление функционала
    Реализация интерфейса I_MessageManager

--- practiceServer/helperClasses/managers/readyReadManager/supportRRManagers/clientsMessageManager/clients_message_manager.cpp
+++ practiceServer/helperClasses/managers/readyReadManager/supportRRManagers/clientsMessageManager/clients_message_manager.cpp
@@ -1,22 +1,44 @@
 #include "clients_message_manager.h"
-#include <QDebug>
 
 ClientsMessageManager::ClientsMessageManager()
 {
 
 }
 
-void ClientsMessageManager::processData(QDataStream &in)
+void ClientsMessageManager::readDataFromStream(QDataStream &inStream)
 {
-    QString str;    //  создаем переменную строки
-    in >> str;  //  записываем в нее строку из объекта in, чтобы проверить содержимое
-//    Server::signalStatusServer("User "+QString::number(socket->socketDescriptor())+" "+socket->localAddress().toString()+": "+str);     //  оформляем чат на стороне Сервера
+    inStream >> this->message;
+    inStream >> this->senderName;
+}
 
-//    SendToAllClients(mapRequest["001"],"<font color = black><\\font>User "+QString::number(socket->socketDescriptor())+" "+socket->localAddress().toString()+": "+str.remove(0,5)+delimiter);      //  мы просто избавляемся от префикса "MESS:" и пересылаем клиенту сообщение
-    qDebug() << "ClientsMessageManager::processData:        " << str;
+void ClientsMessageManager::writeDataToStream(QDataStream &outStream)
+{
+    outStream << this->message;
+    outStream << this->senderName;
+}
+
+void ClientsMessageManager::processData(QDataStream &inStream, QTcpSocket *socket)
+{
+    inStream >> this->message;
+    inStream >> this->senderName;
+    emit signalStatusRRManager("User "+senderName+" "+QString::number(socket->socketDescriptor())+" "+socket->localAddress().toString()+": "+message);     //  оформляем чат на стороне Сервера
+    emit signalSendToAllClientsRRManager("Message","<font color = black><\\font>User "+senderName+" "+QString::number(socket->socketDescriptor())+" "+socket->localAddress().toString()+": "+message.remove(0,5));      //  мы просто избавляемся от префикса "MESS:" и пересылаем клиенту сообщение
+    qDebug() << "ClientsMessageManager::processData:        " << message << "from" << senderName;
 }
 
 QString ClientsMessageManager::typeOfMessage()
 {
     return QString("Message");
+}
+
+QDataStream &operator >>(QDataStream &in, ClientsMessageManager &clientsMessageManager){
+    in >> clientsMessageManager.message;
+    in >> clientsMessageManager.senderName;
+    return in;
+}
+
+QDataStream &operator <<(QDataStream &out, ClientsMessageManager &clientsMessageManager){
+    out << clientsMessageManager.message;
+    out << clientsMessageManager.senderName;
+    return out;
 }
--- practiceServer/helperClasses/managers/readyReadManager/supportRRManagers/clientsMessageManager/clients_message_manager.h
+++ practiceServer/helperClasses/managers/readyReadManager/supportRRManagers/clientsMessageManager/clients_message_manager.h
@@ -4,9 +4,16 @@
 ///     Класс ClientsMessageManager реализует интерфейс I_MessageManager
 ///     обрабатывает всё необходимое, что связанное с обычными сообщениями клиентов
 ///     Переменные:
+///     message - сообщение от клиента
+///     senderName - имя отправителя
 ///     Методы:
+///     readDataFromStream() - чтение данных с потока
+///     writeDataFromStream() - запись данных в поток
 ///     processData() - обрабатывает приходящие данные
 ///     typeOfMessage() - возвращает строку тип менеджера
+///     Сигналы:
+///     signalStatusRRManager() - отправка статуса ReadyReadManager'у
+///     signalSendToOneRRManager() - отправка сообщения одному клиенту через ReadyReadManager
 
 ///  ========================    классы проекта
 #include "../I_message_manager.h"   //  реализуем интерфейс
@@ -16,8 +23,23 @@ class ClientsMessageManager : public I_MessageManager
     Q_OBJECT
 public:
     ClientsMessageManager();
-    void processData(QDataStream &in) override;
+    void readDataFromStream(QDataStream &inStream) override;
+    void writeDataToStream(QDataStream &outStream) override;
+    void processData(QDataStream &inStream, QTcpSocket *socket) override;
     QString typeOfMessage() override;
+
+//  переопределение операторов >> и <<
+protected:
+    friend QDataStream &operator >> (QDataStream &in, ClientsMessageManager &clientsMessageManager);
+    friend QDataStream &operator << (QDataStream &out, ClientsMessageManager &clientsMessageManager);
+
+private:
+    QString message;
+    QString senderName;
+
+signals:
+    void signalStatusRRManager(QString status);
+    void signalSendToAllClientsRRManager(QString typeOfMsg, QString str);
 };
 
 #endif // CLIENTSMESSAGEMANAGER_H
