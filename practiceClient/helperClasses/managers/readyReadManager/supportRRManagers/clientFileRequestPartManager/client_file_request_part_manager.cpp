59ce0280429d08d446343bf71ee0e080253cca3f
KOPOBA <dima.peven@yandex.ru>
2023-05-21 01:01:47 +0300

    Добавление ClientFileRequestPartManager
    Менеджер отвечает за запросы частей файла

---
+++ practiceClient/helperClasses/managers/readyReadManager/supportRRManagers/clientFileRequestPartManager/client_file_request_part_manager.cpp
@@ -0,0 +1,70 @@
+#include "client_file_request_part_manager.h"
+
+ClientFileRequestPartManager::ClientFileRequestPartManager()
+{
+
+}
+
+void ClientFileRequestPartManager::readDataFromStream(QDataStream &inStream)
+{
+
+}
+
+void ClientFileRequestPartManager::writeDataToStream(QDataStream &outStream)
+{
+
+}
+
+void ClientFileRequestPartManager::processData(QDataStream &inStream)
+{
+    QString str;    //  определяем переменную, в которую сохраним уведомление от запроса
+    inStream >> str;  //  выводим в переменную сообщение
+
+    qDebug() << "ClientFileRequestPartManager::processData:     str:" << str;  //  выводим в консоль
+    emit signalStatusRRManager(str);
+
+    qDebug() << "ClientFileRequestPartManager::processData:     file->pos():" << file->pos();
+    if((fileSize - file->pos()) < blockData){   //  если остаток файла будет меньше блока байт
+        blockData = fileSize - file->pos();     //  мы просто будем читать этот остаток
+    }
+
+    bytes = new char[blockData];   //  выделяем байты под файл, то есть передача пройдет в несколько этапов
+
+    qDebug() << "ClientFileRequestPartManager::processData:     read: " << file->read(bytes, blockData);     //  читаем файл и записываем данные в байты
+    qDebug() << "ClientFileRequestPartManager::processData:     blockData: " << blockData;   //  нужно, чтобы видеть текущий размер блоков
+
+    buffer = buffer.fromRawData(bytes, blockData);
+    qDebug() << "ClientFileRequestPartManager::processData:     block size" << blockData << "buffer size" << buffer.size();
+
+    delete[] bytes;
+
+    emit signalSendBufferRRManager(buffer);
+}
+
+QString ClientFileRequestPartManager::typeOfMessage()
+{
+    return QString("Request part of file");
+}
+
+void ClientFileRequestPartManager::setFilePath(QString &filePath)
+{
+    file = new QFile(filePath);   //  определяем файл, чтобы поработать с его свойствами и данными
+    fileSize = file->size();
+    if(!file->open(QIODevice::ReadOnly)){
+        emit signalStatusRRManager("Невозможно открыть файл "+filePath+" для дальнейшей отправки");
+        return;
+    }
+}
+
+void ClientFileRequestPartManager::slotClearFileData()
+{
+    qDebug() << "File "+fileName+" downloaded";   //  выводим консоль, какой файл был загружен
+
+    file->close();  //  закрываем файл
+    delete file; //  удаляем файл
+    file = nullptr; //  удаляем файл
+    fileName.clear();   //  очищаем его название
+    fileSize = 0;   //  очищаем его размер
+    blockData = 1000000;  //  устанавливаем прежний размер байтов
+    delete[] bytes; //  удаляем байты из кучи
+}
---
+++ practiceClient/helperClasses/managers/readyReadManager/supportRRManagers/clientFileRequestPartManager/client_file_request_part_manager.h
@@ -0,0 +1,36 @@
+#ifndef CLIENTFILEREQUESTPARTMANAGER_H
+#define CLIENTFILEREQUESTPARTMANAGER_H
+
+#include "../I_message_manager.h"
+#include <QFile>
+
+class ClientFileRequestPartManager : public I_MessageManager
+{
+    Q_OBJECT
+public:
+    ClientFileRequestPartManager();
+
+    void readDataFromStream(QDataStream &inStream) override;
+    void writeDataToStream(QDataStream &outStream) override;
+    void processData(QDataStream &inStream) override;
+    QString typeOfMessage() override;
+
+    void setFilePath(QString &filePath);
+
+private:
+    int fileSize;   //  размер файла
+    QString fileName;   //  его название
+    QFile *file;     //  сам файлик
+    char *bytes = {0};     //  массив байт данных
+    int blockData = 1000000;  //  размер данных
+    QByteArray buffer;
+
+signals:
+    void signalStatusRRManager(QString status);
+    void signalSendBufferRRManager(QByteArray &buffer);
+
+public slots:
+    void slotClearFileData();
+};
+
+#endif // CLIENTFILEREQUESTPARTMANAGER_H
